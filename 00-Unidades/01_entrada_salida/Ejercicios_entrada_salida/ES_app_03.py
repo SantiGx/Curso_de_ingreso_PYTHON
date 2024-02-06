a030f46eb28407d14b497a3f74dfc9e5b237e7a1
SantiGx <santiagogomez711@gmail.com>
2024-02-06 11:24:20 -0300

    pepepe

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_03.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_03.py
@@ -34,7 +34,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        nombre_alumno = self.txt_nombre.get()
+        alert("Datos", nombre_alumno)
                 
     
 if __name__ == "__main__":
